c3e6620deb75809c3105bf2120ffe9b74b8dde30
Nicolas Castellini <100251427+YoProgramo2022@users.noreply.github.com>
2022-05-03 17:59:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const btnEnvio=document.getElementById("enviarcorreo");
+
+btnEnvio.addEventListener('click', function(e) {
+    e.preventDefault();
+    const nombre=document.getElementById("nombre").value  ;
+    const apellido=document.getElementById("apellido").value;
+    const mensaje=document.getElementById("mensaje").value;
+    windows.location.href='mailto: nicolasrcastellini@gmail.com subject=envioDesdeFormulario&body=nombre%3A${nombre}%0Aapellido%3A&{apellido}%0Amensaje%3A&{mensaje}';
+
+})
+
+
+
+const boton=document.getElementById("lapiz");
+const cursos=document.querySelector(".cursos");
+
+boton.addEventListener ('click', function(){ 
+      const contraseña = prompt("Ingrese la contraseña: ");
+      if(contraseña=="patagonias"){
+        const cursoNuevo = prompt("Ingresar nuevo Curso o Carrera: "); 
+        agregarCurso(cursoNuevo); 
+      }       
+      else
+         alert("Error de Contraseña");       
+})
+
+
+function agregarCurso(cursoNuevo){           
+    li = document.createElement("li");
+    li.textContent = cursoNuevo;
+    cursos.appendChild(li);  
+    };
